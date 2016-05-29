b1b1839b952ff1dcd650eadf654132a032dd1c73
iddinev <xi_dinev@abv.bg>
2016-05-29 21:44:57 +0300

    Week 2 birthday_ranges()

--- week2/problems_week2.py
+++ week2/problems_week2.py
@@ -6,3 +6,12 @@ def hack_dict(*tpls, **as_kw):
         return {key: value for key, value in as_kw.items()}
     else:
         return {tpls[0][i][0]: tpls[0][i][1] for i in range(0, len(tpls[0]))}
+
+
+def birthday_ranges(birthdays, ranges):
+    result = []
+    aux = []
+    for tpl in ranges:
+        aux.append([day for day in birthdays if tpl[0] <= day <= tpl[1]])
+    result.extend([len(aux[i]) for i in range(0, len(aux))])
+    return result
