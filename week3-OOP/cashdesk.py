4b535fc57e44a85883bfba8c6d7d8c9ec1635a4b
Ivan Dinev <xidinev@gmail.com>
2016-06-01 09:58:01 +0300

    Week 3: CashDesk progress

--- week3-OOP/cashdesk.py
+++ week3-OOP/cashdesk.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import textwrap
+
 
 class Bill:
 
@@ -10,6 +12,10 @@ class Bill:
             raise ValueError
         else:
             self._amount = amount
+
+    @property
+    def total(self):
+        return self.__int__()
 
     def __int__(self):
         return self._amount
@@ -27,7 +33,7 @@ class Bill:
         return "{0}({1})".format(self.__class__.__name__, self._amount)
 
 
-class BillBatch:
+class BatchBill:
 
     def __init__(self, bills):
         if not isinstance(bills, list):
@@ -37,14 +43,40 @@ class BillBatch:
     def __len__(self):
         return len(self._bills)
 
+    @property
     def total(self):
-        return sum([int(x) for x in self._bills])
+        return sum([x.total for x in self._bills])
 
     def __getitem__(self, index):
-        return self._bills[index].__str__()
+        return self._bills[index]
 
-    def __repr__(self)
+    def __repr__(self):
+        return "{0}({1})".format(self.__class__.__name__, 'list_of_Bill()')
 
 
 class CashDesk:
-    pass
+
+    def __init__(self):
+        self._pool = []
+
+    def take_money(self, money):
+        if isinstance(money, (Bill, BatchBill)):
+            self._pool.append(money)
+
+    def total(self):
+        return sum([x.total for x in self._pool])
+
+    def inspect(self):
+
+        inspect_string = textwrap.dedent("""
+        We have a total of {0}$ in the desk
+        We have the following count of bills, sorted in ascending order:""".format(self.total()))
+        print(inspect_string)
+
+        bill_nominals = list(set([int(x) for x in self._pool]))
+        bill_nominals.sort()
+        bill_count = []
+        bill_count.extend([self._pool.count(nominal) for nominal in bill_nominals])
+
+        for i in range(0, len(bill_count)):
+            print("{}$ bills - {}".format(bill_nominals[i], bill_count[i]))
