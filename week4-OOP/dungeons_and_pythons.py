012f435f0285b68baee1436d2abaae11feea4731
iddinev <xidinev@gmail.com>
2016-06-06 19:13:22 +0300

    Week 4: Dungeons and Pythons basic methods initial versions (not tested)

--- week4-OOP/dungeons_and_pythons.py
+++ week4-OOP/dungeons_and_pythons.py
@@ -1,78 +1,132 @@
 #!/usr/bin/env python3
 
+
 class GameExceptions(Exception):
+    pass
 
 
-class BaseUnit:
+class _BaseUnit:
 
-    def __init__(self, name=None, title=None, health=0, mana=0,
-                 mana_regeneration_rate=0, weapon=None):
+    def __init__(self, health=0, mana=0, weapon=None, cancast=False):
 
         if health > 0:
             self._maxhealth = health
             self._health = health
         else:
-            pass # Raise exception.
+            pass  # Raise exception.
 
-        if mana > 0:
+        if mana >= 0:
             self._maxmana = mana
             self._mana = mana
         else:
-            pass # Raise exception.
+            pass  # Raise exception.
 
         self._alive = True
-        self._mana_regeneration_rate = mana_regeneration_rate
-
-
+        self._cancast = cancast
 
     def is_alive(self):
-        pass
+        return self._alive
 
     def can_cast(self):
-        pass
+        return self._cancast
 
     def get_health(self):
-        pass
+        return self._health
 
     def get_mana(self):
-        pass
+        return self._mana
 
-    def take_healing(self):
-        pass
+    def take_healing(self, healing_points):
+        if self.is_alive() and healing_points > 0:
+            if healing_points + self.get_health() >= self._maxhealth:
+                self._health = self._maxhealth
+            else:
+                self._health += healing_points
+            return True
+        else:
+            return False
 
-    def take_damage(self):
-        pass
+    def take_damage(self, damage_points):
+        if self.is_alive() and damage_points >= 0:
+            if self.get_health() - damage_points <= 0:
+                self._alive = False
+            else:
+                self._health -= damage_points
 
-    def take_mana(self):
-        pass
+    def take_mana(self, mana_points):
+        if self.is_alive() and mana_points > 0:
+            if self.get_mana() - mana_points <= 0:
+                return False
+            else:
+                self._mana -= mana_points
+            return True
+        else:
+            return False
 
-    def learn(self):
-        pass
+    def learn(self, spell):
+        self._spell = spell
 
-    def equip(self):
-        pass
+    def equip(self, weapon):
+        self._weapon = weapon
 
-    def attack(self):
-        pass
-
-
-class BaseWeapon:
-    pass
+    def attack(self, opponent=None, by=None):
 
 
-class Hero(BaseUnit):
+
+class _BaseWeapon:
+
+    def __init__(self, name=None, damage=0):
+        if damage > 0 and name is not None:
+            self._damage = damage
+            self._name = name
+        else:
+            pass  # Raise exception.
+
+    def get_name(self):
+        return self._name
+
+    def get_damage(self):
+        return self._damage
+
+
+class Hero(_BaseUnit):
 
     def __init__(self, name, title, health, mana, mana_regeneration_rate):
-        self.name = 'wtf'
+        super().__init__(health, mana)
+        self._name = name
+        self._title = title
+        if mana_regeneration_rate > 0:
+            self._mana_regeneration_rate = mana_regeneration_rate
+        else:
+            pass  # Raise exception.
 
     def known_as(self):
-        return "blah"
+        return "{0} the {1}".format(self._name, self._title)
 
-class Enemy(BaseUnit):
+
+class Enemy(_BaseUnit):
+
+    def __init__(self, health, mana, damage):
+        super().__init__(health, mana)
+        self._base_attack = Weapon('Base Attack', damage)
+
+
+class Weapon(_BaseWeapon):
     pass
 
-class Spell(BaseWeapon):
-    pass
 
-class Weapon(BaseWeapon):
-    pass
+class Spell(_BaseWeapon):
+
+    def __init__(self, name, damage, mana_cost=0, cast_range=0):
+        super().__init__(name, damage)
+        if mana_cost >= 0 and cast_range >= 0:
+            self._mana_cost = mana_cost
+            self._cast_range = cast_range
+        else:
+            pass  # Raise exception.
+
+    def get_mana_cost(self):
+        return self._mana_cost
+
+    def get_cast_range(self):
+        return self._cast_range
--- week4-OOP/unittest_dungeons_and_pythons.py
+++ week4-OOP/unittest_dungeons_and_pythons.py
@@ -8,11 +8,9 @@ class TestHero(unittest.TestCase):
 
 
     def testknown_as(self):
-        testobj = Hero(name="Bron", title="Dragonslayer", health=100, mana=100,
+        testobj = Hero(name="Bron", title="Dragonslayer", health=100, mana=200,
                        mana_regeneration_rate=2)
         self.assertEqual("Bron the Dragonslayer", testobj.known_as())
-
-    def test
 
 if __name__ == "__main__":
     unittest.main()
